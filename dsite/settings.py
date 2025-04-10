c99b9eca8ede5cd593ced3fb4aa248e86dee53c8
vic7256 <104175643+vic7256@users.noreply.github.com>
2025-04-10 17:22:04 -0300

    Update settings.py

--- dsite/settings.py
+++ dsite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-vy424nfl!)k+yt1jo02trel4+^$14w92mdk9#pzi5fv2r5izmn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
